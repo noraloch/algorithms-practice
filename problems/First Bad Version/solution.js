36cee54cbfd39dc13f73d834ed357a3dddce0471
Noura <contact.nouraloudani@gmail.com>
2021-04-20 11:54:35 -0400

    first bad version

---
+++ problems/First Bad Version/solution.js
@@ -0,0 +1,19 @@
+
+var solution = function(isBadVersion) {
+
+    return function(n) {
+        let low = 1;
+        let top = n;
+        
+        while (low < top) {
+            const mid = parseInt (low + (top - low) / 2);
+
+            if (isBadVersion(mid)) {
+                top = mid;
+            } else {
+                low = mid + 1;
+            }      
+        }
+        return low;
+    }
+}
