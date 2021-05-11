f84b9dc0e02d97e9bc4516ccea27a481c07b0e6c
Noura <contact.nouraloudani@gmail.com>
2021-05-10 20:20:22 -0400

    new algo - Longest Sequence

---
+++ problems/Longest Sequence/README.md
@@ -0,0 +1,6 @@
+<h2>Longest Sequence</h2>
+given an array of integers find the number with the longest sequence.
+
+example:
+
+	`findMostRepeated([5,5,4,4,4,4,7,7,6,6,6,1,1,1,1,1,1,4,4,4,5,4,4,4]) = 1`
---
+++ problems/Longest Sequence/solution.js
@@ -0,0 +1,34 @@
+function longestSequenceNumber(arr) {
+    let number;
+    let count = 0;
+    let maxNumber;
+    let maxCount = 0;
+
+    arr.forEach((num, i) => {
+        if (number === undefined) {
+            number = num;
+            count++;
+            maxNumber = number;
+        } else if (num === number) {
+
+            if (i === arr.length - 1) {
+                if (maxCount < count) {
+                    maxNumber = number;
+                }
+            } else {
+                count++;
+            }
+
+        } else if (num !== number) {
+            if (maxCount < count) {
+                maxCount = count;
+                maxNumber = number;
+            }
+            number = num;
+            count = 1;
+        }
+    })
+    return maxNumber;
+}
+
+console.log(longestSequenceNumber([4,4,4,4,4,4,2,2,3,4,4,4,4,4,6,6,8,8,8,8,8,8,8,7,8]));
