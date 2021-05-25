5b00b8a14f7d53550c4f1ee583517b63d592babd
Noura <contact.nouraloudani@gmail.com>
2021-05-25 18:05:31 -0400

    one test added

--- problems/Linked-List-Implementation/LinkedList.js
+++ problems/Linked-List-Implementation/LinkedList.js
@@ -9,6 +9,45 @@ class LinkedList {
         this.head = newNode
         this.length ++
     }
+
+    getByIndex(i) {
+        if (i < 0 || i >= this.length) return null
+
+        let currentNode = this.head
+        for (let i = 0 ; i < this.length; i++) {
+            currentNode = currentNode.next
+        }
+        return currentNode
+    }
+
+    insertAtIndex(i, data) {
+        if (index === 0) return this.insertAtHead(data)
+
+        const prevNode = this.getByIndex(i - 1)
+
+        if (prevNode == null) return null
+        
+        prevNode.next == new LinkedList(data, prevNode.next)
+        this.length ++
+    }
+
+    removeHead() {
+        this.head = this.head.next
+        length --
+    }
+
+    removeAtIndex(i) {
+        if (i === 0) return this.removeHead()
+
+        const prevNode = this.getByIndex(i - 1)
+
+        if (prevNode == null) return null
+        
+        prevNode.next = prev.next.next
+
+        this.length -- 
+
+    }
 }
 
 class Node {
--- problems/Linked-List-Implementation/LinkedList.test.js
+++ problems/Linked-List-Implementation/LinkedList.test.js
@@ -1,2 +1,17 @@
 const LinkedList = require("./LinkedList");
 
+describe('#insertAtHead', () => {
+    test('it adds an element to the beginning of the list', () => {
+        const linkedList = new LinkedList()
+        linkedList.insertAtHead('World')
+        oldHead = linkedList.head
+        linkedList.insertAtHead('Hello')
+
+        expect(linkedList.head.value).toBe('Hello')
+        expect(linkedList.head.next).toBe(oldHead)
+        expect(linkedList.length).toBe(2)
+
+
+    })
+    
+});
