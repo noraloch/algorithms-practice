41dd7cf44ee3be1a07dba681c8be3d3473a35f45
Noura <contact.nouraloudani@gmail.com>
2021-05-31 22:18:26 -0400

    done

--- problems/Longest-Substring/solution.js
+++ problems/Longest-Substring/solution.js
@@ -1,3 +1,23 @@
 function lengthOfLongestSubString(string) {
-
+    let finalSeq = "";
+    for (let i = 0; i < string.length; i++) {
+        let tempSeq = "";
+        for (let l = i; l < string.length; l++) {
+            if (!tempSeq.includes(string.charAt(l))) {
+                tempSeq += string.charAt(l);
+                if (l === string.length - 1) {
+                    finalSeq = tempSeq > finalSeq ? tempSeq : finalSeq
+                    if ( i === 0) {
+                        return finalSeq.length;
+                    }
+                }
+            } else {
+                finalSeq = tempSeq > finalSeq ? tempSeq : finalSeq
+                l = string.length
+            }
+        }
+    }
+    return finalSeq.length;
 }
+
+console.log(lengthOfLongestSubString("aaaa"));
