987d7aeb1012e348a3310b1a3be2ee06c4761f89
Noura <contact.nouraloudani@gmail.com>
2021-06-28 23:11:55 -0400

    new problem being solved

---
+++ problems/Merge-Sorted-Arrays/solutions.js
@@ -0,0 +1,26 @@
+function mergeTwo(arr1, arr2) {
+    let mergedArr = [];
+    let index1 = 0;
+    let index2 = 0;
+    let i = 0;
+
+    while (i < ((arr1.length) + (arr2.length))) {
+        let unmerged1 = arr1[index1];
+        let unmerged2 = arr2[index2];
+       
+        if (unmerged1 < unmerged2) {
+            mergedArr[i] = unmerged1;
+            index1++;
+        } else {
+            mergedArr[i] = unmerged2;
+            index2++;
+
+        }
+        i++;
+
+    }
+    return mergedArr.filter(e => e !== 0)
+}
+
+// console.log(mergeTwo([1, 2, 3, 0, 0, 0], [2, 5, 6]))
+
