3d05af584526a7640f6af6401cbb6c42936da5ea
Noura <contact.nouraloudani@gmail.com>
2021-07-08 03:24:38 -0400

    good progress

--- problems/dup/solution.js
+++ problems/dup/solution.js
@@ -1,4 +1,26 @@
 function removeDup(nums) {
     let uniqueNums = [];
+    let bool = [];
+    let prevNum;
 
+
+    nums.forEach(num,i => {
+       '
+       '
+
+        if (uniqueNums.length === 0) {
+            uniqueNums.push(num);
+        }
+        else {
+            if (nums)
+        }
+        // if (prevNum !== num) {
+
+        // }
+        // if (bool[num] === undefined)
+        //     bool[num] = false;
+        // else if (bool[num] === false)
+        //     bool[num] = true;
+        // prevNum = num;
+    })
 }
