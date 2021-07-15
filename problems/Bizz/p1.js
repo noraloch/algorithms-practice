73cf695aeb76eb65b221910ce1c16c5766ae78b4
Noura <contact.nouraloudani@gmail.com>
2021-07-15 17:07:21 -0400

    p1 solution completed

--- problems/Bizz/p1.js
+++ problems/Bizz/p1.js
@@ -1,4 +1,20 @@
-function oddPositionCount() {
-    // print element in odd position with occurence number
+function oddPositionCount(str) {
+    // print element in odd position with occurence number i.e abbloseckc - a:1,b:2,o:1,e:1,k:1
+    let arr = Array.from(str);
+    let result = {};
+    arr.forEach((elm, i) => {
 
+        let count = 0;
+        let position = i + 1;
+
+        if (position % 2 > 0 && !result[elm]) {
+            // console.log(result);
+            arr.forEach(e=> { if(e === elm) count++ })
+            result[elm] = count;
+        }
+    })
+    return result;
 }
+
+// console.log(oddPositionCount("bbbbbjjjjj"));
+
