ec754c5f3f998142d10152d79cfc04b1e4b48a14
Noura <contact.nouraloudani@gmail.com>
2021-07-15 20:08:02 -0400

    fixed edge case

--- problems/Bizz/README.md
+++ problems/Bizz/README.md
@@ -1,11 +1,13 @@
-Some Interview Practice Questions
+## Bi Series
 
-P1
+### P1
 Program to print unique elemets of an string present on odd position, along with the count of those elements in the entire string
 i.e abbloseckc - a:1,b:2,o:1,e:1,k:1
 
-P2
+### P2
 write a fuction that checks if two provided strings are anagrams of each other; letter casing should'nt matter. Also, consider
 only characters, not spaces or punctuation. For example
 function('finder','Friend') -->true
 function('hello','bye') -->false--------**
+
+### P3
--- problems/Bizz/p2.js
+++ problems/Bizz/p2.js
@@ -1,14 +1,27 @@
 function areAnagrams(word1, word2) {
 
-    let w1 = word1.replaceAll(' ', '').toLowerCase();
-    let w2 = word2.replaceAll(' ', '').toLowerCase();
-
+    let w1 = word1.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+    let w2 = word2.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+    let isAnag;
     if (w1.length !== w2.length) return false;
-    else {
-        w1Array = Array.from(w1);
-        w2Array = Array.from(w2);
-        return w1Array.find(char => !w2Array.includes(char)) ? false : true;
+
+    w1Array = Array.from(w1);
+    w2Array = Array.from(w2);
+
+    w1Array.forEach(char => {
+        if (!w2Array.includes(char)) {
+            isAnag = false;
+        } else if (isAnag !== false) {
+            remove(char);
+            isAnag = true;
+        }
+    });
+    function remove(c) {
+        let ind = w2Array.indexOf(c);
+        w2Array.splice(ind, 1);
     }
+
+    return isAnag;
 }
 
-console.log(areAnagrams('f', 'finrders'));
+// console.log(areAnagrams('dss', 'ssd'));
